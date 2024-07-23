6499fd0acf98899507142fd7dcdfc0b800a5401c
codedreammer <nkanal38@gmail.com>
2024-07-23 16:42:38 +0530

    add the file

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+'''dic = {"akshay":"Human being",
+       "Spoon":"object"
+       }
+print(dic["akshay"])
+'''
+info = {'name':'karan','age':19,'eligible':True}
+print(info.items())
+for key, value in info. items():
+    print(f"the value corresponding to the key{key}"
+ is{value})
+    
